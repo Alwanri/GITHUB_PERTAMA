b6c590e51fb23075ffa7af930bdd060187e45ec4
Alwanri <eben15ti@mahasiswa.pcr.ac.id>
2017-10-27 10:08:55 +0700

    inii

--- GITHUB_PERTAMA/GITHUB_EBEN/COBA_GITHUB.aspx.vb
+++ GITHUB_PERTAMA/GITHUB_EBEN/COBA_GITHUB.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Label1.Text = "SELAMAATTxT"
+        Label1.Text = "SELAMAAxxxTTxT"
     End Sub
 
 End Class
