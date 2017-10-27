1bdff5a2385df5c42c49544907282c3a1f3a14a4
Alwanri <eben15ti@mahasiswa.pcr.ac.id>
2017-10-27 10:01:20 +0700

    5222

---
+++ GITHUB_PERTAMA/GITHUB/COBA_GITHUB.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class COBA_GITHUB
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "SELAMATTTTTTTTTTTTT"
+    End Sub
+
+End Class
